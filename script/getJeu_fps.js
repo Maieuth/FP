feeadb7ab444959928172ad965ab72c07a651e65
Maieuth <153730482+Maieuth@users.noreply.github.com>
2025-04-18 22:38:51 +0200

    Add files via upload
    ajout des scripts

---
+++ script/getJeu_fps.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.game-image').forEach(img => {
+    img.addEventListener('click', () => {
+      const nomJeu = img.dataset.nom; 
+  
+      fetch(`../pages/getJeu_fps.php?nom=${encodeURIComponent(nomJeu)}`)
+        .then(response => response.json())
+        .then(data => {
+          
+          document.getElementById('game-info').innerHTML = `
+            <h2>${data.nom}</h2>
+            <p>${data.descrip}</p>
+          `;
+        })
+        .catch(err => console.error('Erreur lors du chargement du jeu :', err));
+    });
+  });
+  
